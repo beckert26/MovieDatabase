765a56e036dda75fea4a6bd5082dbb5d3bc1995c
Brett Eckert <bdem7v@mail.missouri.edu>
2019-11-25 19:35:18 -0600

    3

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <head>
     	<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>Moviee Database</title>
+        <title>Movie Database</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Open+Sans" />
     </head>
